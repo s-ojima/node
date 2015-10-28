2bc78a3300ee2b087370c83a7fedc31d8784fd3d
shinsuke <s-ojima@hermescorp.co.jp>
2015-10-29 04:38:27 +0900

    study

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var connection = require('../public/javascript/mysqlConnect');
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
